bd8fadc2d75db5297051ac87d49b5771db8d8f3e
selaw <nsp.mailku@gmail.com>
2022-01-03 22:47:54 +0700

    first commit ya

---
+++ Assets/Script/navigasi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigasi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ke(int step){
+        //Application.LoadLevel (step);
+        SceneManager.LoadScene(step);
+    }
+}
